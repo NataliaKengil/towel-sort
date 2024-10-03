cf1b5e35a39b51218b426933e888f68ba378e921
NataliaKengil <241283nata@gmail.com>
2024-10-03 20:14:14 +0300

    feat: test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+
+  if (!Array.isArray(matrix) || matrix.length === 0) {
+      return result; // Если матрица не задана или пуста, возвращаем пустой массив
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 0) {
+          // Четные строки: добавляем слева направо
+          result.push(...matrix[i]);
+      } else {
+          // Нечетные строки: добавляем справа налево
+          result.push(...matrix[i].reverse());
+      }
+  }
+
+  return result;
+  
 }
